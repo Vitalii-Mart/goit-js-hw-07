ee06d5e245397b0ed65ba3ccaed08c9afeed603b
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-16 14:50:48 +0200

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markupGallery);
+
+galleryEl.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => document.addEventListener("keydown", onCloseModal),
+      onClose: () => document.removeEventListener("keydown", onCloseModal),
+    }
+  );
+  instance.show();
+
+  function onCloseModal(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
