ef7d981133027637890d83fbfb4898529db1e5e3
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-16 15:29:36 +0200

    02 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markupGallery);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
 
 console.log(galleryItems);
